a0ef2d7386edb731e25f549d4cb9d5fa1c016afb
Arnav Gupta <championswimmer@aokp.co>
2013-08-25 15:31:22 +0530

    Sorting codes written in C++
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

---
+++ CPP_Sorting/CPP_Sorting.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CPP_Sorting.cpp
+// Author      : Arnav Gupta
+// Version     :
+// Copyright   : Copyright (c) 2013 Arnav Gupta, under GNU GPLv3
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
